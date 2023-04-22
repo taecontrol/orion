0f2b65fd14b2af04b12d8da0fb2463c833c8b5d5
Luis Güette <guetteluis@gmail.com>
2023-04-22 09:55:18 -0400

    delete messages when deleting session

--- src-tauri/src/commands/session_commands.rs
+++ src-tauri/src/commands/session_commands.rs
@@ -1,10 +1,11 @@
-use diesel::{QueryDsl, RunQueryDsl};
-use diesel::prelude::*;
-use tauri::Manager;
-use uuid::Uuid;
 use crate::db::establish_db_connection;
 use crate::models::{NewSession, Session};
+use crate::schema::messages::dsl::messages;
 use crate::schema::sessions::dsl::sessions;
+use diesel::prelude::*;
+use diesel::{QueryDsl, RunQueryDsl};
+use tauri::Manager;
+use uuid::Uuid;
 
 #[tauri::command]
 pub fn list_sessions() -> Vec<Session> {
@@ -41,6 +42,11 @@ pub fn new_session() -> Session {
 #[tauri::command]
 pub fn delete_session(app_handle: tauri::AppHandle, parent_session_id: String) {
     let connection = &mut establish_db_connection();
+
+    diesel::delete(messages)
+        .filter(crate::schema::messages::dsl::session_id.eq(parent_session_id.clone()))
+        .execute(connection)
+        .expect("Error deleting messages");
 
     diesel::delete(sessions)
         .filter(crate::schema::sessions::dsl::id.eq(parent_session_id.clone()))
